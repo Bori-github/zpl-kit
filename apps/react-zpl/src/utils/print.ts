189239cacb08a2d58989edb9a59c663b12cc2e6b
Bori-github <qhflrnfl4324@gmail.com>
2025-11-27 16:10:17 +0900

    feat(react-zpl): printChild 헬퍼 함수 추가 및 printChildren 함수 수정 - printChild: React 노드를 재귀적으로 처리하여 ZPL 렌더링에 맞게 자식 노드를 반환하는 헬퍼 함수 구현 - printChildren: printChild를 사용하여 자식 요소를 처리하도록 수정

--- apps/react-zpl/src/utils/print.ts
+++ apps/react-zpl/src/utils/print.ts
@@ -3,6 +3,34 @@ import { isValidElement, ReactNode } from "react";
 import { renderReactElement } from "./render";
 
 /**
+ * 단일 React 노드를 재귀적으로 순회하면서 자식 노드를 ZPL 렌더링 파이프라인에 맞게 처리하는 헬퍼
+ *
+ * @param node - 처리할 React 노드 (ReactNode 타입)
+ * @param _context - ZPL 명령어 생성에 필요한 컨텍스트 정보 (현재 미사용, 향후 개선 예정)
+ * @returns 렌더링된 ReactNode 배열. 유효하지 않은 노드인 경우 `undefined`를 반환합니다.
+ */
+export const printChild = (
+  node: ReactNode,
+  context: any, // TODO: improve context type
+): ReactNode[] | undefined => {
+  if (!isValidElement(node)) return;
+
+  const children: ReactNode[] = [];
+
+  for (const element of renderReactElement(node)) {
+    if (!isValidElement(element)) return;
+
+    // TODO: print element with context
+
+    if (node.props.children) {
+      children.push(printChild(node.props.children, context));
+    }
+  }
+
+  return children;
+};
+
+/**
  * React 노드의 자식 요소들을 렌더링하여 배열로 반환하는 함수
  *
  * 이 함수는 ZPL 라벨 생성 과정에서 React 컴포넌트 트리를 순회하며
@@ -21,15 +49,14 @@ import { renderReactElement } from "./render";
  */
 export const printChildren = (
   node: ReactNode,
-  _context: any, // TODO: improve context type
+  context: any, // TODO: improve context type
 ): ReactNode[] | undefined => {
   if (!isValidElement(node)) return;
 
   const children: ReactNode[] = [];
 
   for (const child of renderReactElement(node)) {
-    // TODO: print child with context
-    children.push(child);
+    children.push(printChild(child, context));
   }
 
   return children;
