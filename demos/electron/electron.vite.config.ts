ff9b72c96784ed139bf7e337f9dcd960ee920d10
Bori-github <qhflrnfl4324@gmail.com>
2025-12-02 23:52:38 +0900

    feat(demos): electron 셋팅

---
+++ demos/electron/electron.vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      target: "node20",
+    },
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      target: "node20",
+    },
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        "@renderer": resolve("src/renderer/src"),
+      },
+    },
+    plugins: [react()],
+    build: {
+      target: "esnext",
+    },
+  },
+});
