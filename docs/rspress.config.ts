180f4101cabf993de5fdefcf6d497f0501ecfa27
Bori-github <qhflrnfl4324@gmail.com>
2026-03-14 20:12:54 +0900

    chore(docs): Rspress v2 file-based nav 적용
    - _meta.json을 _nav.json으로 이름 변경
    - themeConfig.nav, themeConfig.sidebar 제거 (file-based 사용)

--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -4,38 +4,4 @@ export default defineConfig({
   root: 'docs',
   title: 'ZPL Kit',
   description: 'ZPL Kit Documentation',
-  themeConfig: {
-    nav: [
-      {
-        text: 'Guide',
-        link: '/guide/introduction',
-      },
-    ],
-    sidebar: {
-      '/': [
-        {
-          text: 'Getting Started',
-          items: [
-            {
-              text: 'Introduction',
-              link: '/guide/introduction',
-            },
-            {
-              text: 'Getting Started',
-              link: '/guide/getting-started',
-            },
-          ],
-        },
-        {
-          text: 'ZPL Commands',
-          items: [
-            {
-              text: 'Reference',
-              link: '/zpl-commands/reference',
-            },
-          ],
-        },
-      ],
-    },
-  },
 });
